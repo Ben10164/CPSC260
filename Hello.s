329ad2b07e327071b161d443f45813f140044ddc
Ben Puryear <bpuryear@zagmail.gonzaga.edu>
2022-03-21 21:41:00 +0000

    uh idk

--- Hello.s
+++ Hello.s
@@ -8,23 +8,15 @@ LC0:
 	.def	_main;	.scl	2;	.type	32;	.endef
 _main:
 LFB12:
-	.cfi_startproc
 	pushl	%ebp
-	.cfi_def_cfa_offset 8
-	.cfi_offset 5, -8
 	movl	%esp, %ebp
-	.cfi_def_cfa_register 5
 	andl	$-16, %esp
 	subl	$16, %esp
-	call	___main
 	movl	$LC0, (%esp)
 	call	_printf
 	movl	$0, %eax
 	leave
-	.cfi_restore 5
-	.cfi_def_cfa 4, 4
 	ret
-	.cfi_endproc
 LFE12:
 	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
 	.def	_printf;	.scl	2;	.type	32;	.endef
